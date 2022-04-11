eb90b968f8baef98f25fd3157e67f34ffc71e05e
Glitch (glitch-hello-website) <none>
2022-04-11 23:26:31 +0000

    🏜💈 Checkpoint ./Aluno.js:42734116/5657 ./CtrlManterAlunos.js:42734116/2442 ./C trlSessao.js:42734116/453 ./DaoAluno.js:42734116/4866 ./ModelError.js:42734116/248 ./Status.js:42734116/203 ./ViewerAluno.js:42734116/4530 ./ViewerError.js:42734116/253 ./index.html:42734116/5608 ./style.css:42734116/5796

---
+++ CtrlSessao.js
@@ -0,0 +1,34 @@
+"use strict";
+
+import CtrlManterAlunos from "/CtrlManterAlunos.js";
+import Aluno from "/Aluno.js";
+
+export default class CtrlSessao {
+  
+  //-----------------------------------------------------------------------------------------//
+  
+  constructor() {
+    this.ctrlAtual = new CtrlManterAlunos();
+  }
+  
+  //-----------------------------------------------------------------------------------------//
+}
+
+var sessao = new CtrlSessao();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
