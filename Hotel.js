fa6b66933e7aad80193303d73c3e93f4f265e6b2
Glitch (glitch-hello-website) <none>
2022-05-03 05:07:31 +0000

    🏖🌷 Checkpoint ./Hotel.js:42734116/559

--- Hotel.js
+++ Hotel.js
@@ -253,19 +253,12 @@ export default class Hotel {  //ALTERADO NOME DA CLASSE
   //-----------------------------------------------------------------------------------------//
   
   static validarCep(cep) {
-    // Caso o CEP não esteja nesse formato ele é inválido!
-            var objER = /^[0-9]{2}\.[0-9]{3}-[0-9]{3}$/;
-
-            cep = Trim(cep)
-            if(cep.length > 0)
-                {
-                    if(objER.test(cep))
-                        return true;
-                    else
-                        return false;
-                }
-            else
-                return blnVazio; 
+    const regex = /[0-9]{5}-[\d]{3}/g;
+    const cep_ = cep;
+    if (regex.test(cep_)) {
+      return true;
+    }
+    return null;
   }
   
   //-----------------------------------------------------------------------------------------//
